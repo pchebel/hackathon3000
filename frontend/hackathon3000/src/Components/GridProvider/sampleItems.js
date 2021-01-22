6084cad0179e0c928bd0e6fe394877e8f7486445
kThieb <kthiebdev@gmail.com>
2021-01-22 14:57:36 +0100

    ajout de l ajout des widgets

--- frontend/hackathon3000/src/Components/GridProvider/sampleItems.js
+++ frontend/hackathon3000/src/Components/GridProvider/sampleItems.js
@@ -4,39 +4,39 @@ import EmptyWidget from '../EmptyWidget/EmptyWidget';
 
 export default [
   {
-    element: <EmptyWidget />,
+    element: <EmptyWidget id={0} />,
     id: '8c444080-9e02-4946-9205-ea65ed81a66f',
   },
   {
-    element: <EmptyWidget />,
+    element: <EmptyWidget id={1} />,
     id: '0a62f9fa-6f5c-4b09-8ff0-d2b9169426f9',
   },
   {
-    element: <Calendar />,
+    element: <Calendar id={2} />,
     id: '308f0373-a548-4802-8824-a1429ed3e801',
   },
   {
-    element: <EmptyWidget />,
+    element: <EmptyWidget id={3} />,
     id: '3c36057f-11de-4fc0-bc52-334c2339741a',
   },
   {
-    element: <EmptyWidget />,
+    element: <EmptyWidget id={4} />,
     id: '1',
   },
   {
-    element: <EmptyWidget />,
+    element: <EmptyWidget id={5} />,
     id: '2',
   },
   {
-    element: <EmptyWidget />,
+    element: <EmptyWidget id={6} />,
     id: '3',
   },
   {
-    element: <EmptyWidget />,
+    element: <EmptyWidget id={7} />,
     id: '4',
   },
   {
-    element: <EmptyWidget />,
+    element: <EmptyWidget id={8} />,
     id: '5',
   },
 ];
