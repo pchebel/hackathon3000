3eefd5c96380bfdbe1181442519f8de576fab81f
khalilmahfoudh <khalil.mahfoudh@imt-atlantique.net>
2021-01-22 14:17:40 +0100

    Update PostFacebook.jsx

--- frontend/hackathon3000/src/Components/PostFacebook/PostFacebook.jsx
+++ frontend/hackathon3000/src/Components/PostFacebook/PostFacebook.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState, Component } from 'react';
-import { SnackbarContent } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 
 const PostFacebook = () => {
+  const [message, setMessage] = useState('');
   const postFacebook = async () => {
     // POST request using fetch inside useEffect React hook
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: 'Ceci est un test' }),
+      body: JSON.stringify({message}),
     };
     fetch(
-      'https://graph.facebook.com/100421482058128/feed&access_token=EAAGAHDhUTKMBANqxsFGrwowlmUOuEd5CNC1SoWlZBMRSWcmgkZCo4xsTzch8f7udsUsnG2Qo7ISA1i6zUhCygSQnZBeN4b6zUz9nwDCjg05QvHEJTcn5u2eqpfu4vRkN9oqXFzFnySpGAZBK3hrAvFOxI75UnDX2y6tKr77gsr4A7m9liy9H',
+      'https://graph.facebook.com/100421482058128/feed?access_token=EAAGAHDhUTKMBANqxsFGrwowlmUOuEd5CNC1SoWlZBMRSWcmgkZCo4xsTzch8f7udsUsnG2Qo7ISA1i6zUhCygSQnZBeN4b6zUz9nwDCjg05QvHEJTcn5u2eqpfu4vRkN9oqXFzFnySpGAZBK3hrAvFOxI75UnDX2y6tKr77gsr4A7m9liy9H',
       requestOptions
     );
 
@@ -19,6 +20,8 @@ const PostFacebook = () => {
 
   return (
     <div>
+      <TextField id="standard-basic" label="Postez ici" onChange={(event) => setMessage(event.target.value)} />
+      
       <button onClick={postFacebook}>Poster sur Facebook</button>
     </div>
   );
