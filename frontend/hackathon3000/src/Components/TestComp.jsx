b70f349f89fbe2128a2eee47223a5b3191db3f00
khalilmahfoudh <khalil.mahfoudh@imt-atlantique.net>
2021-01-22 02:01:33 +0100

    Changement token

--- frontend/hackathon3000/src/Components/TestComp.jsx
+++ frontend/hackathon3000/src/Components/TestComp.jsx
@@ -4,7 +4,7 @@ const TestComp = () => {
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch(
-        'https://graph.facebook.com/238625004520116?access_token=EAAGAHDhUTKMBAE0D1zO8B1ZAgJWXcOqPZBO2c5Gl8lOQWPpseHHuybylScGhffYZBpOuLsVIhtJjLAo9qUVZAWB9NuyyjPB7P3Cx4d8gdWZCPmSEpGo62yv5MZAy9ePx5k7TZB7D89zBk7lao7y7SPq5w08IZAmd6ae7Eny7Crkg48EI9H2YykZB1VH5fRjcVGlnJkV6wZB5IoUBP84XVZCdTmpGg6fediMlxVifI9KIOsYFwZDZD'
+        'https://graph.facebook.com/238625004520116?access_token=EAAGAHDhUTKMBAJPN464yh8FUyTFe0aSGeZAbkSWmIZBCdo4SkZC8nEQK9HoWQoMlMXgIedXNLvR31Jwd0MgbZBCKmixs6HE2UuBOhhL2tGIuzidKD23ygWZClkabyxbY9vRoyPdLdh0LSHyckSrNVom3mKXZAO0weVtmOqxdZCWbAZDZD'
       );
       const parsedData = await data.json();
       console.log(parsedData);
